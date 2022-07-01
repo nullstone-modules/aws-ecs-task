ad1a385a0db4903bdb1fd9e2802a8eaa82160600
Brad Sickles <brad.sickles@gmail.com>
2022-07-01 08:15:16 -0400

    wip

---
+++ outputs.tf
@@ -0,0 +1,99 @@
+output "region" {
+  value       = data.aws_region.this.name
+  description = "string ||| The region where the ECS container resides."
+}
+
+output "cluster_arn" {
+  value       = data.aws_ecs_cluster.cluster.arn
+  description = "string ||| "
+}
+
+output "log_provider" {
+  value       = local.log_provider
+  description = "string ||| "
+}
+
+output "log_group_name" {
+  value       = module.logs.name
+  description = "string ||| "
+}
+
+output "log_reader" {
+  value       = module.logs.reader
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to read logs from Cloudwatch."
+  sensitive   = true
+}
+
+output "image_repo_name" {
+  value       = try(aws_ecr_repository.this[0].name, "")
+  description = "string ||| "
+}
+
+output "image_repo_url" {
+  value       = try(aws_ecr_repository.this[0].repository_url, "")
+  description = "string ||| "
+}
+
+output "image_pusher" {
+  value = {
+    name       = aws_iam_user.image_pusher.name
+    access_key = aws_iam_access_key.image_pusher.id
+    secret_key = aws_iam_access_key.image_pusher.secret
+  }
+
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to push images."
+
+  sensitive = true
+}
+
+output "deployer" {
+  value = {
+    name       = aws_iam_user.deployer.name
+    access_key = aws_iam_access_key.deployer.id
+    secret_key = aws_iam_access_key.deployer.secret
+  }
+
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to deploy ECS services."
+
+  sensitive = true
+}
+
+output "service_image" {
+  value       = "${local.service_image}:${local.app_version}"
+  description = "string ||| "
+}
+
+output "task_family" {
+  value       = aws_ecs_task_definition.this.family
+  description = "string ||| "
+}
+
+output "main_container_name" {
+  value       = local.container_definition.name
+  description = "string ||| The name of the container definition for the main service container"
+}
+
+output "service_security_group_id" {
+  value       = aws_security_group.this.id
+  description = "string ||| "
+}
+
+output "private_urls" {
+  value       = local.private_urls
+  description = "list(string) ||| A list of URLs only accessible inside the network"
+}
+
+output "public_urls" {
+  value       = local.public_urls
+  description = "list(string) ||| A list of URLs accessible to the public"
+}
+
+output "private_hosts" {
+  value       = local.private_hosts
+  description = "list(string) ||| A list of Hostnames only accessible inside the network"
+}
+
+output "public_hosts" {
+  value       = local.public_hosts
+  description = "list(string) ||| A list of Hostnames accessible to the public"
+}
