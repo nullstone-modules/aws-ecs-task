45f074964be4c509e0c55f72c2deef19b1964475
Brad Sickles <brad.sickles@gmail.com>
2023-04-25 11:22:11 -0400

    Added cluster-namespace connection (#7)

--- cluster.tf
+++ cluster.tf
@@ -1,24 +1,18 @@
-data "ns_connection" "cluster" {
-  name     = "cluster"
-  type     = "cluster/aws-ecs"
-  contract = "cluster/aws/ecs:ec2"
+data "ns_connection" "cluster_namespace" {
+  name     = "cluster-namespace"
+  contract = "cluster-namespace/aws/ecs:*"
 }
 
-data "ns_connection" "network" {
-  name     = "network"
-  type     = "network/aws"
-  contract = "network/aws/vpc"
-  via      = data.ns_connection.cluster.name
+data "ns_connection" "cluster" {
+  name     = "cluster"
+  contract = "cluster/aws/ecs:ec2"
+  via      = data.ns_connection.cluster_namespace.name
 }
 
 locals {
-  vpc_id         = data.ns_connection.network.outputs.vpc_id
-  private_cidrs  = data.ns_connection.network.outputs.private_cidrs
-  public_cidrs   = data.ns_connection.network.outputs.public_cidrs
-  cluster_name   = data.ns_connection.cluster.outputs.cluster_name
-  deployers_name = data.ns_connection.cluster.outputs.deployers_name
-}
-
-data "aws_ecs_cluster" "cluster" {
-  cluster_name = local.cluster_name
+  service_domain       = data.ns_connection.cluster_namespace.outputs.namespace
+  service_discovery_id = data.ns_connection.cluster_namespace.outputs.service_discovery_id
+  cluster_arn          = data.ns_connection.cluster.outputs.cluster_arn
+  cluster_name         = data.ns_connection.cluster.outputs.cluster_name
+  deployers_name       = data.ns_connection.cluster.outputs.deployers_name
 }
