60315f3d8ea5927a6799d0225561b85a90e0830e
Brad Sickles <brad.sickles@gmail.com>
2025-08-19 10:33:02 -0400

    Fixed syntax constructing `linuxParameters`.

--- linux_params.tf
+++ linux_params.tf
@@ -12,6 +12,6 @@ locals {
 locals {
   cap_linux_params     = [for cp in local.cap_modules : lookup(cp.outputs, "linux_parameters", [])]
   special_linux_params = local.kernal_cap_linux_params
-  all_linux_params     = concat(local.cap_linux_params, local.special_linux_params)
-  linux_params         = length(local.all_linux_params) > 0 ? merge(local.all_linux_params...) : null
+  all_linux_params     = flatten(concat(local.cap_linux_params, local.special_linux_params))
+  linux_params         = length(local.all_linux_params) > 0 ? merge([for lp in local.all_linux_params : lp]...) : null
 }
